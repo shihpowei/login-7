834ff8c027f4b0771f40a623e2bdc2483d048257
Ken <kenppoomm0102@gmail.com>
2021-09-16 23:52:23 +0800

    添加JS動畫

---
+++ app.js
@@ -0,0 +1,19 @@
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+const loginpText = document.querySelector(".title-text .login");
+const signupText = document.querySelector(".title-text .signup");
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginpText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+    loginForm.style.marginLeft = "0%";
+    loginpText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+    signupBtn.click();
+    return false;
+});
